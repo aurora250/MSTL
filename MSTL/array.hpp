eef8d13fd4a55096f9587167c879b981883fa52b
aurora250 <146723131+aurora250@users.noreply.github.com>
2025-03-21 12:07:54 +0800

    update optional and variant

---
+++ MSTL/array.hpp
@@ -0,0 +1,8 @@
+//
+// Created by 17379 on 2025/3/19.
+//
+
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+
+#endif //ARRAY_HPP
---
+++ MSTL/optional.hpp
@@ -0,0 +1,8 @@
+//
+// Created by 17379 on 2025/3/19.
+//
+
+#ifndef OPTIONAL_HPP
+#define OPTIONAL_HPP
+
+#endif //OPTIONAL_HPP
